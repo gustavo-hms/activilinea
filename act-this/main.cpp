62a925ea53369a8fd4c3a9f2abd2a8e97a83b894
Gustavo Henrique Montesião de Sousa <gustavo@registro.br>
2012-10-01 11:30:05 -0300

    act-this first version

---
+++ act-this/main.cpp
@@ -0,0 +1,21 @@
+#include <KActivities/Consumer>
+#include <KActivities/Info>
+#include <QCoreApplication>
+#include <QString>
+#include <QTextStream>
+
+QString getActivityName()
+{
+    KActivities::Consumer consumer;
+    KActivities::Info info(consumer.currentActivity());
+    return info.name();
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+    QTextStream out(stdout);
+    out << getActivityName() << endl;
+    
+    return 0;
+}
