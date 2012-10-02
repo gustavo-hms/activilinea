45e841cb004a732d27af4538318f8daef2a374a8
Gustavo Henrique Montesião de Sousa <gustavo@registro.br>
2012-10-02 10:17:38 -0300

    First (non working) version of act-change

---
+++ act-change/main.cpp
@@ -0,0 +1,33 @@
+#include <KActivities/Consumer>
+#include <KActivities/Controller>
+#include <QCoreApplication>
+#include <QString>
+#include <QTextStream>
+
+
+QString findId()
+{
+	KActivities::Consumer consumer;
+	QStringList list = consumer.listActivities();
+	
+	QString id;
+	foreach (id, list) {
+		if (id != consumer.currentActivity()) {
+			break;
+		}
+	}
+	
+	return id;
+}
+
+int main(int argc, char *argv[])
+{
+	QCoreApplication app(argc, argv);
+	
+	
+	QString current = findId();
+	KActivities::Controller controller;
+	controller.setCurrentActivity(current);
+	
+	return 0;
+}
