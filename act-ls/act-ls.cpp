abde073a9917aa290073e610cee07a0a4658b585
Gustavo Henrique Montesião de Sousa <gustavo@registro.br>
2012-10-04 10:20:28 -0300

    First implementation of act-ls

---
+++ act-ls/act-ls.cpp
@@ -0,0 +1,32 @@
+#include <KActivities/Consumer>
+#include <KActivities/Info>
+#include <QCoreApplication>
+#include <QTextStream>
+#include <QString>
+
+QString prettyPrint(KUrl url)
+{
+	return url.prettyUrl();
+}
+
+QString listFiles()
+{
+	KActivities::Consumer consumer;
+	QString current = consumer.currentActivity();
+	KActivities::Info info(current);
+	KUrl::List list = info.linkedResources();
+	QStringList output;
+	foreach (KUrl url, list) {
+		output << prettyPrint(url);
+	}
+	
+	return output.join("\n");
+}
+
+int main(int argc, char *argv[])
+{
+	QCoreApplication app(argc, argv);
+	QTextStream out(stdout);
+	out << listFiles() << endl;
+	return 0;
+}
